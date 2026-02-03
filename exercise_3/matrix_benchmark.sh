432256f556474e527c41b792ef8a34de9855a277
goncalo-meneses <goncalomeneses@tecnico.ulisboa.pt>
2026-02-03 14:45:42 +0100

    Small fixes to batch slurm script

--- exercise_3/matrix_benchmark.sh
+++ exercise_3/matrix_benchmark.sh
@@ -5,6 +5,7 @@
 #SBATCH --time=00:05:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=4GB
 module load 2025
 module load openmpi
@@ -16,5 +17,5 @@ do
     for block_size in 32 64 100
     do
         srun build/power_gpu.x -size $m_size -block_size $block_size > ./step_2/output_s${m_size}_b${block_size}.out
-    end
-end
+    done
+done
