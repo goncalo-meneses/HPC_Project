39620020e6c99325cb706ef81da34f389be5a471
gborgesrodrigu <G.BorgesRodriguesTosteDeMeneses@student.tudelft.nl>
2026-02-04 14:58:31 +0100

    Added bash script for experiment 1

---
+++ exercise_3/shared_v_global.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#SBATCH --job-name="shared_v_global"
+#SBATCH --partition=gpu-a100-small
+#SBATCH --account=Education-EEMCS-Courses-WI4049TU
+#SBATCH --time=00:05:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=1
+#SBATCH --mem-per-cpu=4GB
+module load 2025
+module load openmpi
+module load slurm
+module load cuda
+
+for m_size in 2000 4000 8000 10000
+do
+    srun build/power_gpu_32.x -size $m_size > ./step_1/shared_s${m_size}.out
+    srun build/global_power_gpu.x -size $m_size > ./step_1/global_s${m_size}.out
+done
